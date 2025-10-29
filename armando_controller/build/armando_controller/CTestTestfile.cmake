33073f72fc7a0b8ffada238091fe86d42a428567
Gioak88 <gio.razzino@studenti.unina.it>
2025-10-29 10:48:08 +0000

    Risolto problema submodule: convertito in cartelle normali

---
+++ armando_controller/build/armando_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/ros2_ws/src/armando_controller
+# Build directory: /home/user/ros2_ws/src/armando_controller/build/armando_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
